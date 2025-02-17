55642f02cd62ab464b621bcf2e33032722165825
elsi009 <pengrung3512@gmail.com>
2025-02-17 17:03:47 +0900

    rsp done

--- rsp/index.js
+++ rsp/index.js
@@ -1,76 +1,81 @@
-const root = document.querySelector("#root");
-const title = document.querySelector("body > #root > h1");
-const button = document.querySelector("button");
-const con = document.querySelector(".con");
-
-const message = ["안내면 진다!", "가위", "바위", "보"];
-let msgIndex = 0;
-
-let intervalId = null;
-let time = 0;
-
-const span1 = document.createElement("span");
-const span2 = document.createElement("span");
-
-let imgIndex = 0;
-let imgIndex2 = 2;
-
-const img = document.querySelectorAll("img");
-
 const imgs = [
   "https://cdn-icons-png.flaticon.com/128/15686/15686578.png",
   "https://cdn-icons-png.flaticon.com/128/13992/13992554.png",
   "https://cdn-icons-png.flaticon.com/128/12355/12355903.png",
 ];
 
-img.forEach((i, index) => (i.src = imgs[index]));
-img[0].src = imgs[0];
+const messages = ["안내면 진다!", "가위", "바위", "보!"];
 
-root.append(span1);
-root.append(span2);
+const generateRandomNumber = () => Math.floor(Math.random() * 3);
 
-button.onclick = () => {
-  title.innerText = message[msgIndex];
-  if (msgIndex === message.length) {
-    return (title.innerText = "이겼다!");
+let imgIndex1 = generateRandomNumber();
+let imgIndex2 = generateRandomNumber();
+
+console.log(imgIndex1, imgIndex2);
+
+let time = 0;
+let index = 0;
+
+const root = document.querySelector("#root");
+const con = document.querySelector(".con");
+const h1 = document.querySelector("h1");
+const button = document.querySelector("button");
+const images = document.querySelectorAll("img");
+
+images.forEach((image, index) => (image.src = imgs[index]));
+
+// 0 가위
+// 1 바위
+// 2 보
+
+const whoWins = (v1, v2) => {
+  if (v1 === v2) {
+    return "비김";
   }
-  msgIndex += 1;
 
-  intervalId = setInterval(() => {
-    console.log("time is ticking...");
-    time += 0.25;
-
-    if (time >= 3) {
-      clearInterval(intervalId);
-    }
-    if (imgIndex === 2) {
-      imgIndex = 0;
-    } else {
-      imgIndex += 1;
-    }
-    if (imgIndex2 === 2) {
-      imgIndex2 = 0;
-    } else {
-      imgIndex2 += 1;
-    }
-    span1.innerText = time;
-
-    const n1 = Math.floor(Math.random() * 3);
-    const n2 = Math.floor(Math.random() * 3);
-
-    imgIndex = n1;
-    imgIndex2 = n2;
-
-    img[0].src = imgs[imgIndex];
-    img[2].src = imgs[imgIndex2];
-  }, 250);
-
-  con.innerHTML = null;
-  const h2 = document.createElement("h2");
-  h2.innerText = "VS";
-  con.append(img[0], h2, img[2]);
+  if (v1 === 0 && v2 === 1) {
+    return "패배...";
+  }
+  if (v1 === 1 && v2 === 2) {
+    return "패배...";
+  }
+  if (v1 === 2 && v2 === 0) {
+    return "패배...";
+  }
+  return "승리!";
 };
 
-const number = Math.floor(Math.random() * 3);
+button.onclick = () => {
+  const titleId = setInterval(() => {
+    console.log("1초 지남");
+    h1.innerText = messages[index];
+    index += 1;
+    time += 1;
+    if (index === 4) {
+      clearInterval(titleId);
+    }
+  }, 1000);
 
-console.log(number);
+  const imgId = setInterval(() => {
+    imgIndex1 = generateRandomNumber();
+    imgIndex2 = generateRandomNumber();
+    images[0].src = imgs[imgIndex1];
+    images[2].src = imgs[imgIndex2];
+    if (index === 4) {
+      clearInterval(imgId);
+      h1.innerText = whoWins(imgIndex1, imgIndex2);
+      button.innerText = "한 판 더!";
+      index = 0;
+      imgIndex1 = generateRandomNumber();
+      imgIndex2 = generateRandomNumber();
+      clearInterval(titleId);
+      clearInterval(imgId);
+    }
+  }, 100);
+
+  con.innerHTML = null;
+
+  const h2 = document.createElement("h2");
+  h2.innerText = "VS";
+  con.append(images[0], h2, images[2]);
+};
